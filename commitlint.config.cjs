1949befa784d47a42c281fd1209b9df8fb0bae98
maxgsandre <mgsa.gustavo@gmail.com>
2025-10-03 19:17:07 -0300

    chore(ci): configurar commitlint e husky para validar mensagens de commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,12 @@
+/**
+ * Commitlint configuration to enforce Conventional Commits.
+ * Examples:
+ *  - feat: add new payment modal
+ *  - fix(dividas): corrigir cálculo de parcela do mês
+ *  - chore: update dependencies
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
+
+
