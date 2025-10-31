79cb20fdddd73ad5a2a2c1314fc08402ff2dbd80
maxgsandre <mgsa.gustavo@gmail.com>
2025-10-31 00:22:12 -0300

    Revert "chore(commitlint): aumentar header-max-length para 120 e flexibilizar regras"
    This reverts commit 357b16a953c29539ec4398eb9b03e216ef1dc8d2.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -7,13 +7,6 @@
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    // Permitir cabeçalhos maiores (até 120 chars)
-    'header-max-length': [2, 'always', 120],
-    // Permitir escopo opcional e assuntos em qualquer caso
-    'scope-empty': [0],
-    'subject-case': [0],
-  },
 };
 
 
