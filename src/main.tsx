e1e402ee13f6d64a5c615069c18d2a33558ead37
maxgsandre <mgsa.gustavo@gmail.com>
2026-03-31 20:24:29 -0300

    feat(pwa): registrar service worker no bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { registerPWA } from "./pwa";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+registerPWA();
+
+createRoot(document.getElementById("root")!).render(<App />);
   
